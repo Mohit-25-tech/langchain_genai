3d96c6ad95b17edfa223a80cf67270d610aaf0ff
Mohit Nirmal <mohitsnirmal2511@gmail.com>
2026-07-08 23:08:48 +0530

    t

--- chatbot/practise.py
+++ chatbot/practise.py
@@ -1,3 +1,9 @@
-import langchain.agents as agents
+from dotenv import load_dotenv
+import os
 
-print(dir(agents))
+load_dotenv()
+
+print("Tracing:", os.getenv("LANGSMITH_TRACING"))
+print("Endpoint:", os.getenv("LANGSMITH_ENDPOINT"))
+print("Project:", os.getenv("LANGSMITH_PROJECT"))
+print("API Key exists:", os.getenv("LANGSMITH_API_KEY") is not None)
